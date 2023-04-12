97e1761036f5199e99bfa50e9e2aab167103e86e
arrpiita sub <arpita.sub@gmail.com>
2023-04-12 05:39:10 +0000

    Wed 12 Apr 2023 05:39:10 AM UTC

--- client/src/components/HomePage.js
+++ client/src/components/HomePage.js
@@ -16,7 +16,7 @@ const HomePage = () => {
 
   <div className='logo'>
 
-    <span><h1></h1></span>
+    <span><h1><img src="https://images.vexels.com/content/144524/preview/library-logo-template-design-20587d.png" /></h1></span>
 
   </div>
 

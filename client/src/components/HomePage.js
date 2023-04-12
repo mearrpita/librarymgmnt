8b342721883420bc07a2fe8e35374bfd73bc5882
arrpiita sub <arpita.sub@gmail.com>
2023-04-12 06:59:10 +0000

    Wed 12 Apr 2023 06:59:10 AM UTC

--- client/src/components/HomePage.js
+++ client/src/components/HomePage.js
@@ -15,7 +15,7 @@ const HomePage = () => {
 <div className='container'>
 
   <div className='logo'>
-  <i class="fa-regular fa-book-open-cover"></i>
+  <span> <h2>Library Management</h2> </span>
 
     {/* <img src="https://images.vexels.com/content/144524/preview/library-logo-template-design-20587d.png" 
     height={50}
@@ -49,6 +49,24 @@ const HomePage = () => {
   </div>
 </div>
 </nav>
+
+ {/* hero-section  */}
+
+ 
+<div className="p-5 text-center bg-image rounded-3">
+  <img src='./image/library-animate.svg' alt=''/>
+  <div className="mask" >
+    <div className="d-flex justify-content-center align-items-center h-100">
+      <div className="text-white">
+        <h1 className="mb-3">Heading</h1>
+        <h4 className="mb-3">Subheading</h4>
+        <a className="btn btn-outline-light btn-lg" href="#!" role="button">Call to action</a>
+      </div>
+    </div>
+  </div>
+</div>
+
+
   
   </>
    

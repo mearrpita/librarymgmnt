f55438e3d3225f85d3bdbc672134e114714559c6
arrpiita sub <arpita.sub@gmail.com>
2023-04-15 05:51:44 +0000

    Sat 15 Apr 2023 05:51:44 AM UTC

--- client/src/components/HomePage.js
+++ client/src/components/HomePage.js
@@ -59,9 +59,7 @@ const HomePage = () => {
   <div className="mask" >
     <div className="d-flex justify-content-center align-items-center h-100">
       <div className="text-white">
-        <h1 className="mb-3">Heading</h1>
-        <h4 className="mb-3">Subheading</h4>
-        <a className="btn btn-outline-light btn-lg" href="#!" role="button">Call to action</a>
+        <h1 className="mb-3"><span>Library Management System</span></h1>
       </div>
     </div>
   </div>

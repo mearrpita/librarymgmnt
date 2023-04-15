715a70b69b7027cf9cfe390cdf7cb7c125afc91c
arrpiita sub <arpita.sub@gmail.com>
2023-04-15 06:46:07 +0000

    Sat 15 Apr 2023 06:46:07 AM UTC

--- client/src/components/HomePage.js
+++ client/src/components/HomePage.js
@@ -69,24 +69,26 @@ const HomePage = () => {
   <p><h2 className='text-left'>Here some of our <span>library pictures:</span></h2></p>
     <div class="container-fluid">
             <div class="row" id="port2-second-row">
-                <div class="col-xs-6 imgcont" id="port2-first-col">
-                    <img src="https://assignmentpoint.com/wp-content/uploads/2013/06/Online-Library.jpg" alt=" " height={300} width={400} padding={0} />
+            <div class="col-xs-6 imgcont" id="port2-second-col">
+                    <img src="https://miro.medium.com/v2/resize:fit:1200/0*36G2SMPQf5XiM05A" alt=" " padding={0} height={300} width={400} />
                 </div>
                 <div class="col-xs-6 imgcont" id="port2-second-col">
                     <img src="https://mir-s3-cdn-cf.behance.net/projects/404/b41900117083619.Y3JvcCwxMzgwLDEwODAsMjcwLDA.jpg" alt=" " padding={0} height={300} />
                 </div>
                 <div class="col-xs-6 imgcont" id="port2-second-col">
-                    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRMTasNQph6qk0lkSPdu1c4BSMuKimif62KAujICU2FLr7-KKj5YYuvmG9sc3rU8jc3zRo&usqp=CAU" alt=" " padding={0} height={300} />
+                    <img src="https://itechindia.co/wp-content/uploads/2021/11/main-banner1.jpeg"alt=" " padding={0} height={300} width={400} />
                 </div>
-                <div class="col-xs-6 imgcont" id="port2-second-col">
-                    <img src="https://miro.medium.com/v2/resize:fit:1200/0*36G2SMPQf5XiM05A" alt=" " padding={0} height={300} width={400} />
+                <div class="col-xs-6 imgcont" id="port2-first-col">
+                    <img src="https://assignmentpoint.com/wp-content/uploads/2013/06/Online-Library.jpg" alt=" " height={300} width={400} padding={0} />
                 </div>
+               
                 <div class="col-xs-6 imgcont" id="port2-second-col">
                     <img src="https://youcanlearnhowtocode.com/wp-content/uploads/2021/03/woman-looking-at-books-in-library-668x525.jpg" alt=" " padding={0} height={300} />
                 </div>
                 <div class="col-xs-6 imgcont" id="port2-second-col">
-                    <img src="https://itechindia.co/wp-content/uploads/2021/11/main-banner1.jpeg"alt=" " padding={0} height={300} width={400} />
+                    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRMTasNQph6qk0lkSPdu1c4BSMuKimif62KAujICU2FLr7-KKj5YYuvmG9sc3rU8jc3zRo&usqp=CAU" alt=" " padding={0} height={300} />
                 </div>
+                
                 
             </div>
        </div>

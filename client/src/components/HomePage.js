6fecd5ddb504e249aed58c96fb1989d47a5db87d
arrpiita sub <arpita.sub@gmail.com>
2023-04-15 07:33:52 +0000

    Sat 15 Apr 2023 07:33:52 AM UTC

--- client/src/components/HomePage.js
+++ client/src/components/HomePage.js
@@ -41,7 +41,8 @@ const HomePage = () => {
         <Link to='/create-book'><b>BOOK</b></Link>
       </li>
       <li>
-        <Link to='/content'><b>ABOUT</b></Link>
+      <a class = "nav-link" href ="#content">HOME</a>
+       {/* <Link to='/content'><b>ABOUT</b></Link> */}
       </li>
       <li>
         <Link to='/'><b>CONTACT-US</b></Link>

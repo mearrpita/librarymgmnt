1ec42bd296c35c9fd45995d6f47cbe048df04e12
arrpiita sub <arpita.sub@gmail.com>
2023-04-15 07:30:49 +0000

    Sat 15 Apr 2023 07:30:49 AM UTC

--- client/src/components/HomePage.js
+++ client/src/components/HomePage.js
@@ -41,7 +41,7 @@ const HomePage = () => {
         <Link to='/create-book'><b>BOOK</b></Link>
       </li>
       <li>
-        <Link to='/'><b>ABOUT</b></Link>
+        <Link to='/content'><b>ABOUT</b></Link>
       </li>
       <li>
         <Link to='/'><b>CONTACT-US</b></Link>
@@ -65,15 +65,13 @@ const HomePage = () => {
   </div>
 </div>
 
+<section id="content">
 <div className="p-5 text-center bg-image rounded-3">
   <p><h2 className='text-left'>Here some of our <span>library pictures:</span></h2></p>
     <div class="container-fluid">
             <div class="row" id="port2-second-row">
             <div class="col-xs-6 imgcont" id="port2-second-col">
                     <img src="https://miro.medium.com/v2/resize:fit:1200/0*36G2SMPQf5XiM05A" alt=" " padding={0} height={300} width={400} />
-                    {/* <a href="#!">
-    <div class="mask" style="background-color: hsla(195, 83%, 58%, 0.2)"></div>
-  </a> */}
                 </div>
                 <div class="col-xs-6 imgcont" id="port2-second-col">
                     <img src="https://mir-s3-cdn-cf.behance.net/projects/404/b41900117083619.Y3JvcCwxMzgwLDEwODAsMjcwLDA.jpg" alt=" " padding={0} height={300} />
@@ -91,11 +89,10 @@ const HomePage = () => {
                 <div class="col-xs-6 imgcont " id="port2-second-col">
                     <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRMTasNQph6qk0lkSPdu1c4BSMuKimif62KAujICU2FLr7-KKj5YYuvmG9sc3rU8jc3zRo&usqp=CAU" alt=" " padding={0} height={300} width={400} />
                 </div>
-                
-                
-            </div>
+             </div>
        </div>
 </div>
+</section>
 
 
 
